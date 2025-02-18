f6aa1ee6d851dee0e88f4e835ea2f36e57d231ac
Wellik Perroni <welliksp@gmail.com>
2025-02-18 11:17:30 -0300

    initial commit

---
+++ src/test/java/br/com/wsp/msorder/MsorderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wsp.msorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
